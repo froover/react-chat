e1087a05f30b3bf2c0a0e344b28155dd66f3e00e
froover <dio22038@gmail.com>
2019-04-06 18:58:17 +0900

    一旦コミット

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import registerServiceWorker from './serviceWorker';
+// import registerServiceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import AppChat from './components/AppChat';
-import InjectTapEventPlugin from 'react-tap-event-plugin';
+// import InjectTapEventPlugin from 'react-tap-event-plugin';
 
 ReactDOM.render(<AppChat />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
+
+// registerServiceWorker();
